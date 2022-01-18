8af57e8be305ece6d7845d23ac48c4096644997d
stuart-rickard <stuart@bau-dev.com>
2022-01-18 06:31:25 -0800

    database connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const mysql = require('mysql2');
 const express = require('express');
+const unpublic = require('./unpublic');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +9,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: unpublic(),
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+  
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
