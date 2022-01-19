fe8bc7bd4095b8dfac55fa29f81f1adee8129c94
stuart-rickard <stuart@bau-dev.com>
2022-01-19 14:40:44 -0800

    updated get routes SQL queries

--- server.js
+++ server.js
@@ -26,8 +26,12 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get( '/api/candidates', ( req, res ) => {
-    const sql = `SELECT * FROM candidates`;
-
+  const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
+    
     db.query( sql, ( err, rows ) => {
         if ( err ) {
             res.status( 500 ).json({ error: err.message });
@@ -42,8 +46,13 @@ app.get( '/api/candidates', ( req, res ) => {
 
 // GET a single candidate
 app.get( '/api/candidate/:id', ( req, res ) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
-    const params = [ req.params.id ];
+  const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
+  const params = [ req.params.id ];
     
     db.query( sql, params, ( err, row ) => {
         if ( err ) {
