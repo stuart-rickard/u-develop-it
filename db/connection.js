f9eba91d3767abacfae25b5da7f0f1e5d664a6de
stuart-rickard <stuart@bau-dev.com>
2022-01-19 21:05:08 -0800

    modularized api calls

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const unpublic = require('./unpublic');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: unpublic(),
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+module.exports = db;
