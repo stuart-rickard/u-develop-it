a63e51a501dc86ce2f0454ebf530475205ff4cd4
stuart-rickard <stuart@bau-dev.com>
2022-01-19 21:32:54 -0800

    added voter routes

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 
 const apiRoutes = require('./routes/apiRoutes');
 
-const db = require('./db/connection');
-const inputCheck = require('./utils/inputCheck');
+// const db = require('./db/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
